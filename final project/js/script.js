16c7e111c5e7396c80542d983ca797c072336362
navneetkaur269 <54474356+navneetkaur269@users.noreply.github.com>
2019-09-22 13:33:52 -0400

    PROJECT

---
+++ final project/js/script.js
@@ -0,0 +1,8 @@
+function outputname(){
+    var x,y, name;
+    x= document.getElementById("form1");
+    y= x.elements["name"].value;
+        document.write("the name is "+y+ "</br>");
+     
+
+}
